a16599369a97ea663ede9a434c4ab2c4f17c1bad
carletto19 <luis.mariles@uabc.edu.mx>
2021-05-07 17:25:41 -0700

    Second chunk of angular files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC7R26oDvr2wnZzchleF4gqrktd-GkMSik",
+    authDomain: "april-tests.firebaseapp.com",
+    projectId: "april-tests",
+    storageBucket: "april-tests.appspot.com",
+    messagingSenderId: "899259352248",
+    appId: "1:899259352248:web:a91d43f7de35a0c104204d",
+    measurementId: "G-DW9D154QR7"
+  }
+};
